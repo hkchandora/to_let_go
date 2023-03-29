0a4f18002cb9a82c69c728096f3f03695083bdb6
himanshukumawat <himanshu.kumawat@atriina.com>
2023-03-29 16:44:35 +0530

    Added colors file

---
+++ lib/util/colors.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+
+const appTheme = Color(0xff1B2E6A);
+const colorDarkRed = Color(0xffEB5E50);
+const colorRed = Color(0xffEECAC5);
+const colorLightRed = Color(0xffFFF1EF);
+const colorWhite = Color(0xffFFFFFF);
+const colorBg = Color(0xffF8F9FE);
+const colorLightBlue = Color(0xffE2ECFF);
+const colorDarkGray = Color(0xff333333);
+const colorLightGray = Color(0xff595e68);
+const colorGrey = Color(0xff949DB2);
+const colorBlack = Color(0xff000000);
+const colorGreen = Color(0xff12B529);
+const colorLightGreen = Color(0xffE2FFE6);
+const colorDarkYellow = Color(0xffffe36a);
+const colorLightYellow = Color(0xffFFF8E8);
+const colorLightSky = Color(0xffE7EBFB);
+const colorDisable = Color(0xffdee0e0);
