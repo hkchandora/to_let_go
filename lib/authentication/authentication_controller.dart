fab058f3e7fe64008361ba73f19b6063bb6d247d
himanshukumawat <himanshu.kumawat@atriina.com>
2023-03-31 17:58:56 +0530

    Sign up integration done with firebase

--- lib/authentication/authentication_controller.dart
+++ lib/authentication/authentication_controller.dart
@@ -1,7 +1,13 @@
 import 'dart:io';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:get/get.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:to_let_go/global.dart';
+import 'package:to_let_go/model/user.dart' as userModel;
+import 'package:to_let_go/on_boarding/login_screen.dart';
 
 class AuthenticationController extends GetxController{
 
@@ -35,7 +41,40 @@ class AuthenticationController extends GetxController{
   }
 
 
-  void createAccountForNewUser(File imageFile, String userName, String userEmail, String userPassword) {
+  void createAccountForNewUser(File imageFile, String userName, String userEmail, String userPassword) async {
+    try{
+      UserCredential credential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
+          email: userEmail,
+          password: userPassword);
 
+      String imageDownloadUrl = await uploadImageToStorage(imageFile);
+
+      userModel.User user = userModel.User(
+          name: userName,
+          email: userEmail,
+          image: imageDownloadUrl,
+          uid: credential.user!.uid
+      );
+
+      await FirebaseFirestore.instance.collection("users")
+          .doc(credential.user!.uid).set(user.toJson());
+      Get.snackbar("Account Creation Successful","");
+    } catch (error){
+      Get.snackbar("Account Creation Unsuccessful","Error occurred while creating account. Try Again.");
+      showProgressBar = false;
+      Get.to(LoginScreen());
+    }
+  }
+
+  Future<String> uploadImageToStorage(File imageFile) async {
+    Reference reference = FirebaseStorage.instance.ref()
+        .child("Profile Images")
+        .child(FirebaseAuth.instance.currentUser!.uid);
+
+    UploadTask uploadTask = reference.putFile(imageFile);
+    TaskSnapshot taskSnapshot = await uploadTask;
+
+    String downloadedUrlOfImage = await taskSnapshot.ref.getDownloadURL();
+    return downloadedUrlOfImage;
   }
 }
