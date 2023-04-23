767bfc4e9c09230f372c090118a4e0af86fe5f85
himanshukumawat <himanshu.kumawat@atriina.com>
2023-04-23 22:54:04 +0530

    Added me screen

---
+++ lib/home/profile/account_setting.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AccountSetting extends StatefulWidget {
+  const AccountSetting({Key? key}) : super(key: key);
+
+  @override
+  State<AccountSetting> createState() => _AccountSettingState();
+}
+
+class _AccountSettingState extends State<AccountSetting> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/util/asset_image_path.dart
+++ lib/util/asset_image_path.dart
@@ -1,4 +1,10 @@
 class AssetImagePath {
   static const appLogo = 'images/app_logo.png';
   static const profileAvatar = 'images/profile_avatar.jpg';
+  static const backButton = 'images/back_button.png';
+  static const facebook = 'images/facebook.png';
+  static const instagram = 'images/instagram.png';
+  static const twitter = 'images/twitter.jpg';
+  static const whatsapp = 'images/whatsapp.png';
+  static const youtube = 'images/youtube.png';
 }
