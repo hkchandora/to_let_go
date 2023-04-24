52aed9558fbbe8e7bc543f01b0f051ed0a1e2d7f
himanshukumawat <himanshu.kumawat@atriina.com>
2023-04-24 17:53:36 +0530

    Show grid view

--- lib/home/profile/profile_screen.dart
+++ lib/home/profile/profile_screen.dart
@@ -71,137 +71,150 @@ class _ProfileScreenState extends State<ProfileScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: profileAppBar(),
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const SizedBox(height: 20),
-            Center(
-              child: userProfileImage != null ? CircleAvatar(
-                radius: 80,
-                backgroundImage: NetworkImage(userProfileImage!),
-                backgroundColor: colorBlack,
-              ) : const CircleAvatar(
-                radius: 80,
-                backgroundImage: AssetImage(AssetImagePath.profileAvatar),
-                backgroundColor: colorBlack,
-              ),
-            ),
-            const SizedBox(height: 40),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 50),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: const [
-                      Text("3"),
-                      Text("Following"),
-                    ],
-                  ),
+      body: schemeSelectionBody(),
+    );
+  }
 
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: const [
-                      Text("4"),
-                      Text("Followers"),
-                    ],
-                  ),
-
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: const [
-                      Text("100"),
-                      Text("Likes"),
-                    ],
-                  ),
-                ],
+  Widget schemeSelectionBody() {
+    return NestedScrollView(
+      physics: const NeverScrollableScrollPhysics(),
+      headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+        return <Widget>[
+          SliverList(
+            delegate: SliverChildListDelegate([
+              const SizedBox(height: 20),
+              Center(
+                child: userProfileImage != null ? CircleAvatar(
+                  radius: 80,
+                  backgroundImage: NetworkImage(userProfileImage!),
+                  backgroundColor: colorBlack,
+                ) : const CircleAvatar(
+                  radius: 80,
+                  backgroundImage: AssetImage(AssetImagePath.profileAvatar),
+                  backgroundColor: colorBlack,
+                ),
               ),
-            ),
-            const SizedBox(height: 30),
-            SingleChildScrollView(
-              scrollDirection: Axis.vertical,
-              child: Padding(
+              const SizedBox(height: 40),
+              Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 50),
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    facebookUrl != null && facebookUrl!.isNotEmpty ? GestureDetector(
-                      onTap: () => Utility.launchGivenUrl(facebookUrl!),
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(14),
-                        child: Image.asset(AssetImagePath.facebook, fit: BoxFit.fill, width: 50, height: 50),
-                      ),
-                    ) : const SizedBox(),
-                    instagramUrl != null && instagramUrl!.isNotEmpty ? GestureDetector(
-                      onTap: () => Utility.launchGivenUrl("https://www.instagram.com/$instagramUrl"),
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(14),
-                        child: Image.asset(AssetImagePath.instagram, fit: BoxFit.fill, width: 50, height: 50),
-                      ),
-                    ) : const SizedBox(),
-                    whatsappUrl != null && whatsappUrl!.isNotEmpty ? GestureDetector(
-                      onTap: () => Utility.launchGivenUrl("https://wa.me/+91$whatsappUrl"),
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(14),
-                        child: Image.asset(AssetImagePath.whatsapp, fit: BoxFit.fill, width: 50, height: 50),
-                      ),
-                    ) : const SizedBox(),
-                    twitterUrl != null && twitterUrl!.isNotEmpty ? GestureDetector(
-                      onTap: () => Utility.launchGivenUrl(twitterUrl!),
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(14),
-                        child: Image.asset(AssetImagePath.twitter, fit: BoxFit.fill, width: 50, height: 50),
-                      ),
-                    ) : const SizedBox(),
-                    youtubeUrl != null && youtubeUrl!.isNotEmpty ? GestureDetector(
-                      onTap: () => Utility.launchGivenUrl(youtubeUrl!),
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(14),
-                        child: Image.asset(AssetImagePath.youtube, fit: BoxFit.fill, width: 50, height: 50),
-                      ),
-                    ) : const SizedBox(),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const [
+                        Text("3"),
+                        Text("Following"),
+                      ],
+                    ),
+
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const [
+                        Text("4"),
+                        Text("Followers"),
+                      ],
+                    ),
+
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const [
+                        Text("100"),
+                        Text("Likes"),
+                      ],
+                    ),
                   ],
                 ),
               ),
-            ),
-            const SizedBox(height: 20),
-            GestureDetector(
-              onTap: () => logoutConfirmDialog("Sign Out", "Do you want to sign out?"),
-              child: Container(
-                width: double.infinity,
-                margin: const EdgeInsets.symmetric(horizontal: 50),
-                padding: const EdgeInsets.symmetric(vertical: 6),
-                decoration: BoxDecoration(
-                  border: Border.all(color: colorDarkRed, width: 1),
-                  borderRadius: const BorderRadius.all(Radius.circular(40)),
-                ),
-                child: const Center(
-                  child: Text("Sign Out", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16)),
+              const SizedBox(height: 30),
+              SingleChildScrollView(
+                scrollDirection: Axis.vertical,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 50),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      facebookUrl != null && facebookUrl!.isNotEmpty ? GestureDetector(
+                        onTap: () => Utility.launchGivenUrl(facebookUrl!),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(14),
+                          child: Image.asset(AssetImagePath.facebook, fit: BoxFit.fill, width: 50, height: 50),
+                        ),
+                      ) : const SizedBox(),
+                      instagramUrl != null && instagramUrl!.isNotEmpty ? GestureDetector(
+                        onTap: () => Utility.launchGivenUrl("https://www.instagram.com/$instagramUrl"),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(14),
+                          child: Image.asset(AssetImagePath.instagram, fit: BoxFit.fill, width: 50, height: 50),
+                        ),
+                      ) : const SizedBox(),
+                      whatsappUrl != null && whatsappUrl!.isNotEmpty ? GestureDetector(
+                        onTap: () => Utility.launchGivenUrl("https://wa.me/+91$whatsappUrl"),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(14),
+                          child: Image.asset(AssetImagePath.whatsapp, fit: BoxFit.fill, width: 50, height: 50),
+                        ),
+                      ) : const SizedBox(),
+                      twitterUrl != null && twitterUrl!.isNotEmpty ? GestureDetector(
+                        onTap: () => Utility.launchGivenUrl(twitterUrl!),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(14),
+                          child: Image.asset(AssetImagePath.twitter, fit: BoxFit.fill, width: 50, height: 50),
+                        ),
+                      ) : const SizedBox(),
+                      youtubeUrl != null && youtubeUrl!.isNotEmpty ? GestureDetector(
+                        onTap: () => Utility.launchGivenUrl(youtubeUrl!),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(14),
+                          child: Image.asset(AssetImagePath.youtube, fit: BoxFit.fill, width: 50, height: 50),
+                        ),
+                      ) : const SizedBox(),
+                    ],
+                  ),
                 ),
               ),
+              const SizedBox(height: 20),
+              GestureDetector(
+                onTap: () => logoutConfirmDialog("Sign Out", "Do you want to sign out?"),
+                child: Container(
+                  width: double.infinity,
+                  margin: const EdgeInsets.symmetric(horizontal: 50),
+                  padding: const EdgeInsets.symmetric(vertical: 6),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: colorDarkRed, width: 1),
+                    borderRadius: const BorderRadius.all(Radius.circular(40)),
+                  ),
+                  child: const Center(
+                    child: Text("Sign Out", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16)),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 10),
+            ]),
+          )
+        ];
+      },
+      body: Column(
+        children: <Widget>[
+          const Divider(color: colorWhite, thickness: 1),
+          Expanded(
+            child: GridView.builder(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3, mainAxisSpacing: 2, crossAxisSpacing: 2, mainAxisExtent: 250
+              ),
+              itemCount: 5,
+              itemBuilder: (BuildContext context, int index) {
+                return Card(
+                  color: Colors.white,
+                  child: SizedBox(height: 500,child: Text('$index')),
+                );
+              },
             ),
-            const SizedBox(height: 10),
-            const Divider(color: colorWhite, thickness: 1),
-            // Expanded(
-            //   child: GridView.count(
-            //     crossAxisCount: 3,
-            //     crossAxisSpacing: 4.0,
-            //     mainAxisSpacing: 8.0,
-            //     children: List.generate(3, (index) {
-            //       return const Center(
-            //         child: Text("text", style: TextStyle(color: colorWhite)),
-            //       );
-            //     }
-            //     ),
-            //   ),
-            // ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
