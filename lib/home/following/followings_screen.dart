5df9f2a145c79f1506ce6d1eef04f61df055fade
himanshukumawat <himanshu.kumawat@atriina.com>
2023-05-26 15:00:51 +0530

    get all following list

--- lib/home/following/followings_screen.dart
+++ lib/home/following/followings_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:to_let_go/home/following/following_controller.dart';
 import 'package:to_let_go/util/style.dart';
 
 class FollowingsScreen extends StatefulWidget {
@@ -9,15 +11,29 @@ class FollowingsScreen extends StatefulWidget {
 }
 
 class _FollowingsScreenState extends State<FollowingsScreen> {
+
+  FollowingController followingController = Get.put(FollowingController());
+  List videoList = [];
+
+  @override
+  void initState() {
+    getAllFollowingVideoData();
+    super.initState();
+  }
+
+  getAllFollowingVideoData() async {
+    videoList = await followingController.getAllFollowingVideoDataList();
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-       body: Center(
-           child: Text(
-             "Followings video screen",
-             style: extraBoldTextStyleWhite_18,
-           ),
-       ),
+      body: Center(
+        child: Text(
+          "Followings video screen",
+          style: extraBoldTextStyleWhite_18,
+        ),
+      ),
     );
   }
 }
